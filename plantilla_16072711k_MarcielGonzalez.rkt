b7eea866445281c8947f44a99cbb4ff79add67a5
Hugo Marciel <hugo.marciel@usach.cl>
2023-03-26 20:44:49 -0300

    Incorpora TDA filesystem

---
+++ plantilla_16072711k_MarcielGonzalez.rkt
@@ -0,0 +1,44 @@
+#lang racket
+
+;;;Dependencias de otros archivos(Rquire)
+;;(require "")
+
+;;Funciones que comparto(Provide)
+;;(provide)
+
+;;TDA FILE_SYSTEM ()
+;;Representacion
+;;Descripcion:
+
+
+;;Constructor :
+;;Funcion:
+;;Descripcion:
+;;Dom:
+;;REC:
+
+;;Pertenencia
+;;Funcion:
+;;Descripcion:
+;;Dom:
+;;REC:
+
+;;Selectores
+;;Funcion:
+;;Descripcion:
+;;Dom:
+;;REC:
+
+;;Modificador
+;;Funcion:
+;;Descripcion:
+;;Dom:
+;;REC:
+
+;;Otras funciones
+;;Funcion:
+;;Descripcion:
+;;Dom:
+;;REC:
+
+
